c49e3e77d1c372e3597e09e60e9865f187f1dd24
Konstantin S. <havendv@gmail.com>
2026-05-10 07:30:15 +0400

    feat: Updated OpenAPI spec (#6)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/DashScope/Generated/DashScope.JsonConverters.OneOf2.g.cs
+++ src/libs/DashScope/Generated/DashScope.JsonConverters.OneOf2.g.cs
@@ -107,7 +107,10 @@ namespace DashScope.JsonConverters
                 catch (global::System.InvalidOperationException)
                 {
                 }
+            }
 
+            if (value1 == null && value2 == null)
+            {
                 try
                 {
 

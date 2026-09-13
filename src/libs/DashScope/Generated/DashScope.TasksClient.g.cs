25b7db160fdef36f873fe42db33b043bef3e89d0
Konstantin S. <havendv@gmail.com>
2026-09-13 19:46:52 +0400

    feat: Updated OpenAPI spec (#27)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/DashScope/Generated/DashScope.TasksClient.g.cs
+++ src/libs/DashScope/Generated/DashScope.TasksClient.g.cs
@@ -34,7 +34,7 @@ namespace DashScope
         /// <inheritdoc/>
         public global::DashScope.AutoSDKClientOptions Options { get; }
 
-        internal global::System.Lazy<global::System.Text.Json.Serialization.JsonSerializerContext> JsonSerializerContextProvider { get; set; } = new(() => global::DashScope.SourceGenerationContext.Default);
+        internal global::System.Lazy<global::System.Text.Json.Serialization.JsonSerializerContext> JsonSerializerContextProvider { get; set; } = new(() => global::DashScope.TasksSourceGenerationContext.Default);
 
         /// <summary>
         ///
